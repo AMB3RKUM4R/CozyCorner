a3e40e84f7e554b5ac23d2bd96180ae4b83ce8d0
Amber Kumar <amb3rkum4r@gmail.com>
2025-08-26 17:47:46 +0530

    ghh

--- components/main/StarCanvas.tsx
+++ components/main/StarCanvas.tsx
@@ -1,61 +1,28 @@
-"use client"
+'use client';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
 
-import React, { useState, useRef, Suspense } from 'react'
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
-    import { ThreeElements } from '@react-three/fiber';
-// @ts-ignore
-import * as random from "maath/random/dist/maath-random.esm";
-  declare global {
-      namespace React {
-        namespace JSX {
-          interface IntrinsicElements extends ThreeElements {}
-        }
-      }
-    }
-
-const StarBackground = (props: any) => {
-    const ref: any = useRef(undefined);
-    const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
-
-    useFrame((state, delta) => {
-        ref.current.rotation.x -= delta / 10;
-        ref.current.rotation.y -= delta / 15;
-
-    })
-
-    return (
-        <group rotation={[0, 0, Math.PI / 4]}>
-            <Points
-                ref={ref}
-                positions={sphere}
-                stride={3}
-                frustumCulled
-                {...props}
-            >
-                <PointMaterial
-
-                    transparent
-                    color="#fff"
-                    size={0.002}
-                    sizeAttenuation={true}
-                    dethWrite={false}
-                />
-            </Points>
-        </group>
-    )
-}
+const Stars = () => {
+  // Your star field logic here
+  return (
+    <Points>
+      <PointMaterial />
+    </Points>
+  );
+};
 
 const StarCanvas = () => {
-    return (
-        <div className='w-full h-auto fixed inset-0 z-[-1]'> {/* Changed z-[20] to z-[-1] */}
-            <Canvas camera={{ position: [0, 0, 1] }}>
-                <Suspense fallback={null}>
-                    <StarBackground />
-                </Suspense>
-            </Canvas>
-        </div>
-    )
-}
+  return (
+    <div style={{ width: '100%', height: '100vh', position: 'absolute' }}>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Stars />
+          <Preload all />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
 
-export default StarCanvas
+export default StarCanvas;
